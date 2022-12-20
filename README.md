341b4d961984d2b56ca15c03d5216f35887990d8
ba_c <swunhtetoo@gmail.com>
2022-12-20 17:07:55 +0630

    completing layout

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 * Simple JavaFX application written to learn JavaFX.
 * Funtionality is pretty much the same as a game I played 15 years ago.
 
-* Started on Dec 20, 2:30 PM.
+* Started on Dec 20, 2:30 PM. Completed layout (undocumented) on 5:05PM.
--- tanks/src/main/java/fun/swunoo/Main.java
+++ tanks/src/main/java/fun/swunoo/Main.java
@@ -2,12 +2,7 @@ package fun.swunoo;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application{
